39f715e7901343e9e33dcdfc5b35f6c372a0173a
RomagnaMarco <37676804+RomagnaMarco@users.noreply.github.com>
2020-04-15 21:19:11 -0700

    Update PatientView.java

--- PatientView.java
+++ PatientView.java
@@ -1,1 +1,24 @@
+package stuff;
 
+public class PatientView {
+
+	private int orderNum;
+	private int sequenceNum;
+	
+	public PatientView() {}
+	
+	public void printPatientDetails(int orderNum)
+	{
+		
+	}
+	
+	public void printDataBase()
+	{
+		
+	}
+	
+	public void printVisitDetails(int orderNum, int sequenceNum) //only for visits that have sequence numbers
+	{
+		
+	}
+}
