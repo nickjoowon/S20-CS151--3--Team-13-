faec2ecc0a79dba837090a2941515ce8c85dab77
Nicolas Swanson <nickjoowon@gmail.com>
2020-04-15 21:31:45 -0700

    Update Medicine.java

--- Medicine.java
+++ Medicine.java
@@ -1,1 +1,67 @@
 
+/*
+ * Medicine class deals with the specificity of the medication information. 
+ * This class is about medical history.
+ */
+
+public class Medicine {
+	
+	
+	private String medicineName;
+	private String doseSize; 
+	private String duration;
+	private String chemCategoryName;
+	private String action;
+	private String application;
+	private String usualDose;
+	private boolean sideEffect;
+	
+	
+	public Medicine(String medicineName, String doseSize, String duration, String chemCategoryName, String action, String application, String usualDose, boolean sideEffect) {
+		
+		this.medicineName = medicineName;
+		this.doseSize = doseSize;
+		this.duration = duration;
+		this.chemCategoryName = chemCategoryName;
+		this.action = action;
+		this.usualDose = usualDose;
+		this.sideEffect = sideEffect;
+	}
+
+	
+	/*
+	 * Getters and setter methods
+	 */
+	public String getMedicineName() {
+		return medicineName;
+	}
+
+	public String getDoseSize() {
+		return doseSize;
+	}
+
+	public String getDuration() {
+		return duration;
+	}
+
+	public String getChemCategoryName() {
+		return chemCategoryName;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public String getApplication() {
+		return application;
+	}
+
+	public String getUsualDose() {
+		return usualDose;
+	}
+
+	public boolean isSideEffect() {
+		return sideEffect;
+	}
+	
+	
