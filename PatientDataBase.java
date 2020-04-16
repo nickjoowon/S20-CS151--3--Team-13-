921d3c1b8490598d24f58db6226b8059b0b19d0a
RomagnaMarco <37676804+RomagnaMarco@users.noreply.github.com>
2020-04-15 21:43:54 -0700

    Update PatientDataBase.java

--- PatientDataBase.java
+++ PatientDataBase.java
@@ -8,18 +8,24 @@ public class PatientDataBase {
 	private ArrayList<Patient> patientList;
 	
 	public PatientDataBase() {}
-	
+	/**
+	 *  takes neccesary fields from a patient's info and adds a patient to the database
+	 */
 	public void addPatient(int socialSecureNum,int insuranceNum, int dateAdded, String fullName, int birthDay, String gender, int phoneNum,
 			String address, ArrayList<Medicine> medicineList, boolean hyperacusis, boolean tinnitus, String hypercusisCause, String tinnitusCause)
 	{
 		
 	}
-	
+	/**
+	 *  deletes a specific patient
+	 */
 	public void deletePatient(int patientNum)
 	{
 		
 	}
-	
+	/**
+	 *  deletes last added patient (if they hit undo upon registering a patient)
+	 */
 	public void deleteMostRecentPatient()
 	{
 		
