047a555c9d6b8aadde09ef0c61ef1dcc25dceefd
RomagnaMarco <37676804+RomagnaMarco@users.noreply.github.com>
2020-04-15 21:34:55 -0700

    Update PatientDataBase.java

--- PatientDataBase.java
+++ PatientDataBase.java
@@ -1,1 +1,28 @@
 
+package stuff;
+
+import java.util.ArrayList;
+
+public class PatientDataBase {
+
+	private ArrayList<Patient> patientList;
+	
+	public PatientDataBase() {}
+	
+	public void addPatient(int socialSecureNum,int insuranceNum, int dateAdded, String fullName, int birthDay, String gender, int phoneNum,
+			String address, ArrayList<Medicine> medicineList, boolean hyperacusis, boolean tinnitus, String hypercusisCause, String tinnitusCause)
+	{
+		
+	}
+	
+	public void deletePatient(int patientNum)
+	{
+		
+	}
+	
+	public void deleteMostRecentPatient()
+	{
+		
+	}
+	
+}
