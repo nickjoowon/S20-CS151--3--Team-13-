373d2b98aaafe29890f46fa3332f665914d08eea
Nick Swanson <nickjoowon@gmail.com>
2020-05-13 20:21:51 -0700

    adding packages and sorting the classes

---
+++ src/View/PatientView.java
@@ -0,0 +1,97 @@
+package view;
+/**
+ * PatientView is in charge of displaying things to the user.
+ *
+ */
+public class PatientView {
+
+	public PatientView() {}
+	/**
+	 * sets current Page
+	 */
+	public void setEnum(Page newPage)
+	{
+		
+	}
+	/**
+	 * picks correct Page based on Page Enum currently on.
+	 */
+	public void pickPage()
+	{
+		
+	}
+	/**
+	 * below methods are the UI for a specific page
+	 * they are called from within the PickPage method
+	 */
+	public void displayRegInputReq()
+	{
+		
+	}
+	public void displayRegInputOpt()
+	{
+		
+	}
+	public void displayRegInputStatus()
+	{
+		
+	}
+	public void displayRegInputMedHist()
+	{
+		
+	}
+	
+	public void displayRegVisitInfo()
+	{
+		
+	}
+	public void displayRegAudioEval()
+	{
+		
+	}
+	public void displayRegCategory()
+	{
+		
+	}
+	
+	public void displayMainMenu()
+	{
+		
+	}
+	public void displayPatientDatabase()
+	{
+		
+	}
+	public void displaySpecificPatient()
+	{
+		
+	}
+	public void displayReverseReg()
+	{
+		
+	}
+	public void displayMedHistory()
+	{
+		
+	}
+	public void displaySpecMedInfo()
+	{
+		
+	}
+	public void displayVisitHistory()
+	{
+		
+	}
+	public void displaySpecificVisit()
+	{
+		
+	}
+	public void displayAudioEval()
+	{
+		
+	}
+	public void displayScheduleVisitPopup()
+	{
+		
+	}
+}
