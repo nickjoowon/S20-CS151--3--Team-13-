959970194177497aa477a55724a85ce216c1ff70
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2020-05-14 16:53:58 -0700

    Update PatientController.java

--- CS151_Proj/src/Control/PatientController.java
+++ CS151_Proj/src/Control/PatientController.java
@@ -1,5 +1,40 @@
 package Control;
 
 public class PatientController {
+  
+  static JFrame frame = new JFrame();
+	public static void main(String[] args)
+	{
+		
+		PatientView view = new PatientView(frame); 
+		RegisterListener r = new RegisterListener(); 
+		view.addRegisterListener(r); 
+	
+	}
+	
+	static class RegisterListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			Patient2 page2 = new Patient2(frame); 
+			NextListener n = new NextListener();
+			page2.addNextListener(n);
+		}
+	}
+	static class NextListener implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			PatientView view = new PatientView(frame); 
+			
+			RegisterListener r = new RegisterListener(); 
+			view.addRegisterListener(r); 
+			
+		}
+		
+	}
+		
+	}
+  
 
 }
