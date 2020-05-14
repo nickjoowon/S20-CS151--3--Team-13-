2e90332a940cce18437c8b1877c9610d69527821
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2020-05-14 16:42:58 -0700

    Update MenuPage.java

--- CS151_Proj/src/View/MenuPage.java
+++ CS151_Proj/src/View/MenuPage.java
@@ -1,5 +1,64 @@
 package View;
 
 public class MenuPage {
+  private JButton registerButton; 
+	private JButton viewButton; 
+	private JFrame frame;
+	public MenuPage(JFrame frame)
+	{
+		this.frame = frame; 
+	}
+	
+	public void drawPage()
+	{
+		frame.getContentPane().removeAll();
+		registerButton = new JButton("Register New Patient");
+		viewButton = new JButton("View Patients");
+		frame.setTitle("Main Menu"); 
+		frame.setSize(910,700); 
+		JPanel register = new JPanel(); 
+		register.setSize(455, 700);
+		frame.add(register, BorderLayout.LINE_START); 
+		
+		
+		registerButton.setPreferredSize(new Dimension(455, 665));
+		registerButton.setOpaque(true);
+		registerButton.setBackground(new Color(41, 142, 208));
+		registerButton.setForeground(Color.white);
+		registerButton.setBorderPainted(false);
+		registerButton.setFont(new Font("Arial", Font.BOLD, 40));
+		register.add(registerButton); 
+		
+		JPanel view = new JPanel(); 
+		view.setOpaque(true);
+
+		view.setSize(455, 700);
+		frame.add(view, BorderLayout.CENTER);
+		
+		
+		viewButton.setPreferredSize(new Dimension(435, 665));
+		viewButton.setOpaque(true);
+		viewButton.setBackground(new Color(41, 142, 208));
+		viewButton.setForeground(Color.white);
+		viewButton.setBorderPainted(false);
+		viewButton.setFont(new Font("Arial", Font.BOLD, 40));
+		view.add(viewButton); 
+		
+		
+		frame.setVisible(true); 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.repaint();
+		frame.revalidate();
+		
+	}
+	
+	public void addRegisterListener(ActionListener listenRegister)
+	{
+		registerButton.addActionListener(listenRegister);
+	}
+	public void addViewListener(ActionListener listenView)
+	{
+		viewButton.addActionListener(listenView);
+	}
 
 }
