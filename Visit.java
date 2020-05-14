b53dfe0e5874f56aaec9b3a28105f462e2e537f8
Nicolas Swanson <nickjoowon@gmail.com>
2020-05-13 19:27:43 -0700

    Update Visit.java

--- Visit.java
+++ Visit.java
@@ -1,4 +1,5 @@
-package stuff;
+package Model;
+
 /**
  * The visit class deals with the visits the patients schedule
  * They can contain evaluations, which happen during the patient's visit
@@ -10,79 +11,93 @@ public class Visit {
 	 * orderNum is the patients identification number
 	 */
 	private String fullName;
-	private int date;
-	private int visitNum;
-	private int orderNum;
-	private Evaluations evaluations;
+	private String date;
+	private int sequenceNum;
+	private int IDNum;
+	private Evaluation evaluations;
+	
 	/**
-	 *  takes in full name of the patient, the date of the visit, and the orderNum of the patient
-	 *  
 	 * @param fullName
 	 * @param date
 	 * @param orderNum
 	 */
-	public Visit(String fullName, int date, int orderNum) {
+	public Visit(String fullName, String date, int IDNum, int sequenceNum) {
 		this.fullName = fullName;
 		this.date = date;
-		this.orderNum = orderNum;
-		
+		this.IDNum = IDNum;
+		this.sequenceNum = sequenceNum;
 	}
-/**
- * gets order number
- * @return
- */
-	public int getOrderNum() {
-		return orderNum;
+	
+	
+	/**
+	 * gets patient ID number
+	 * @return patient's ID
+	 */
+	public int IDNum() {
+		return IDNum;
 	}
-/**
- * sets order number
- * @param orderNum
- */
-	public void setOrderNum(int orderNum) {
-		this.orderNum = orderNum;
+	
+	
+	/**
+	 * sets patient ID number
+	 * @param number to set patient's ID number to
+	 */
+	public void setIDNum(int x) {
+		this.IDNum = x;
 	}
-/**
- *  gets full name
- * @return
- */
+	
+	
+	/**
+	 * @return full name
+	 */
 	public String getFullName() {
 		return fullName;
 	}
-/**
- * sets full name
- * @param fullName
- */
+	
+	
+	/**
+	 * sets full name
+	 * @param fullName
+	 */
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
-/**
- * gets date of visit
- * @return
- */
-	public int getDate() {
+	
+	
+	/**
+	 * gets date of visit
+	 * @return
+	 */
+	public String getDate() {
 		return date;
 	}
-/**
- * sets date of visit
- * @param date
- */
-	public void setDate(int date) {
-		this.date = date;
-	}
-/**
- * get number of the visit
- * @return
- */
-	public int getVisitNum() {
-		return visitNum;
-	}
-/**
- * set number of the visit
- * @param visitNum
- */
-	public void setVisitNum(int visitNum) {
-		this.visitNum = visitNum;
+	
+	
+	/**
+	 * sets date of visit
+	 * @param date
+	 */
+	public void setDate(String s) {
+		this.date = s;
 	}
 	
 	
+	/**
+	 * get number of the visit
+	 * @return visit number
+	 */
+	public int getSeqNum() {
+		return sequenceNum;
+	}
+	
+	
+	/**
+	 * set number of the visit
+	 * @param visitNum
+	 */
+	public void setSeqNum(int x) {
+		this.sequenceNum = x;
+	}
+
+
 }
