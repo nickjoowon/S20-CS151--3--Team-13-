c518075760664b133c6471589815b440e218736d
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2020-05-13 21:52:47 -0700

    Update PatientView.java

--- CS151_Proj/src/View/PatientView.java
+++ CS151_Proj/src/View/PatientView.java
@@ -1,5 +1,18 @@
 package View;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 /**
