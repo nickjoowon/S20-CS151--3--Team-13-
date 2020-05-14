62a090c6506a5dd86fad8fa1da2695627621cfe4
johnsonhsiung <47260592+johnsonhsiung@users.noreply.github.com>
2020-05-13 20:27:16 -0700

    Update PatientView.java

--- src/View/PatientView.java
+++ src/View/PatientView.java
@@ -14,7 +14,42 @@ public class PatientView {
 	 */
 	public void pickPage()
 	{
+		SwingUtilities.updateComponentTreeUI(this);
+		registerButton = new JButton("Register New Patient");
+		viewButton = new JButton("View Patients");
+		setTitle("Main Menu"); 
+		setSize(910,700); 
+		JPanel register = new JPanel(); 
+		register.setSize(455, 700);
+		add(register, BorderLayout.LINE_START); 
 		
+		
+		registerButton.setPreferredSize(new Dimension(455, 665));
+		registerButton.setOpaque(true);
+		registerButton.setBackground(new Color(41, 142, 208));
+		registerButton.setForeground(Color.white);
+		registerButton.setBorderPainted(false);
+		registerButton.setFont(new Font("Arial", Font.BOLD, 40));
+		register.add(registerButton); 
+		
+		JPanel view = new JPanel(); 
+		view.setOpaque(true);
+
+		view.setSize(455, 700);
+		add(view, BorderLayout.CENTER);
+		
+		
+		viewButton.setPreferredSize(new Dimension(435, 665));
+		viewButton.setOpaque(true);
+		viewButton.setBackground(new Color(41, 142, 208));
+		viewButton.setForeground(Color.white);
+		viewButton.setBorderPainted(false);
+		viewButton.setFont(new Font("Arial", Font.BOLD, 40));
+		view.add(viewButton); 
+		
+		
+		setVisible(true); 
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	/**
 	 * below methods are the UI for a specific page
