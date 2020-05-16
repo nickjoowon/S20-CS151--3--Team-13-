1f8d811585717218beac9cf5a082bc6b70a3d41a
Marco Romagna <marco.romagna@yahoo.com>
2020-05-15 17:39:19 -0700

    Added to patientdatabase
    added buttons and scrollpane.
    work is needed.
    the list in the scroll pane needs input from controller which gets it
    rom model

--- CS151_Proj/src/View/PatientDataPage.java
+++ CS151_Proj/src/View/PatientDataPage.java
@@ -18,6 +18,11 @@ public class PatientDataPage {
     JFrame frame; 
 	JButton next;
 	JButton back;
+	JButton info;
+	JButton addVisit;
+	JButton delete;
+	JButton schedule;
+	JButton history;
 	
 	
 	public PatientDataPage(JFrame frame)
@@ -64,6 +69,59 @@ public class PatientDataPage {
 				next.setBorderPainted(false);
 				backAndNew.add(next, BorderLayout.EAST); 
 				
+			//list of users
+			String patients[] = {" Temp 1 "," Temp 2 "," Temp 3 "," Temp 4 "," Temp 5 "," Temp 6 "}; //get this from model
+			//note that a space b4 and after information makes it look nicer and less crowded.
+			JList list = new JList(patients);
+			list.setFont(new Font("Arial", Font.PLAIN, 30));
+			JScrollPane scrollBar = new JScrollPane(list);
+			
+			//for buttons regarding patients
+			JPanel patientButtons = new JPanel();
+			patientButtons.setLayout(new GridLayout(5,2));
+			
+			info = new JButton("Info"); 
+			info.setPreferredSize(new Dimension(125,50));
+			info.setOpaque(true);
+			info.setForeground(Color.white);
+			info.setBackground(new Color(41,142,208));
+			info.setBorderPainted(false);
+			patientButtons.add(info, BorderLayout.WEST);
+			
+			addVisit = new JButton("+ Add Visit"); 
+			addVisit.setPreferredSize(new Dimension(125,50));
+			addVisit.setOpaque(true);
+			addVisit.setForeground(Color.white);
+			addVisit.setBackground(new Color(41,142,208));
+			addVisit.setBorderPainted(false);
+			patientButtons.add(addVisit, BorderLayout.WEST);
+			
+			history = new JButton("Visit History"); 
+			history.setPreferredSize(new Dimension(125,50));
+			history.setOpaque(true);
+			history.setForeground(Color.white);
+			history.setBackground(new Color(41,142,208));
+			history.setBorderPainted(false);
+			patientButtons.add(history, BorderLayout.WEST);
+			
+			schedule = new JButton("Schedule Visit"); 
+			schedule.setPreferredSize(new Dimension(125,50));
+			schedule.setOpaque(true);
+			schedule.setForeground(Color.white);
+			schedule.setBackground(new Color(41,142,208));
+			schedule.setBorderPainted(false);
+			patientButtons.add(schedule, BorderLayout.WEST);
+			
+			delete = new JButton("Delete"); 
+			delete.setPreferredSize(new Dimension(125,50));
+			delete.setOpaque(true);
+			delete.setForeground(Color.white);
+			delete.setBackground(new Color(41,142,208));
+			delete.setBorderPainted(false);
+			patientButtons.add(delete, BorderLayout.WEST);
+			
+			
+			
 			// For space between buttons and bottom 
 			JPanel bottomDecorationWhite = new JPanel();
 				
@@ -72,6 +130,8 @@ public class PatientDataPage {
 			//add the panels into the frame 
 			frame.add(top, BorderLayout.NORTH); 
 			frame.add(backAndNew, BorderLayout.SOUTH); 
+			frame.add(scrollBar, BorderLayout.WEST);
+			frame.add(patientButtons, BorderLayout.CENTER);
 			
 			frame.repaint();
 			frame.revalidate();
