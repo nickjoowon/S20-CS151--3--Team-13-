353575cfb8ee6b67faf998a6d47a1fd695ad88e4
RomagnaMarco <37676804+RomagnaMarco@users.noreply.github.com>
2020-05-19 17:08:27 -0700

    added popup method

--- CS151_Project/src/view/PatientDataPage.java
+++ CS151_Project/src/view/PatientDataPage.java
@@ -259,5 +259,47 @@ public class PatientDataPage {
 							//so this should hopefully never be reached
 		
 	}
-	
+	public void addPopup()
+	{
+		//adds popup frame
+		 JFrame popframe = new JFrame("Schedule a Visit");
+		 popframe.setLayout(new GridLayout(2,1));
+		 
+		 JLayeredPane popup = new JLayeredPane();
+		 popframe.getContentPane().add(popup);
+		 
+		 //makes button to update information
+		 JButton enter = new JButton("Enter");
+		 enter.setFont(new Font("Arial", Font.PLAIN, 45));
+		 
+		 //text fields user has to enter to schedule an appointment
+		 JPanel fields = new JPanel();
+		 fields.setLayout(new GridLayout(3,2));
+		 
+		 JLabel date = new JLabel(" Date: ");
+		 date.setFont(new Font("Arial", Font.PLAIN, 20));
+		 fields.add(date);
+		 JTextField dateText = new JTextField();
+		 fields.add(dateText);
+		 
+		 JLabel start = new JLabel(" Start Time: ");
+		 start.setFont(new Font("Arial", Font.PLAIN, 20));
+		 fields.add(start);
+		 JTextField startText = new JTextField();
+		 fields.add(startText);
+		 
+		 JLabel end = new JLabel(" End Time: ");
+		 end.setFont(new Font("Arial", Font.PLAIN, 20));
+		 fields.add(end);
+		 JTextField endText = new JTextField();
+		 fields.add(endText);
+		 
+		 
+		 //add components to frame
+		 popframe.add(fields);
+		 popframe.add(enter, BorderLayout.EAST);
+		 
+		 popframe.setSize (800,600);
+		 popframe.setVisible(true); 
+	}
 }
